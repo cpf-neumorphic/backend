bf760edd633ac6fdb9a424fe27cdebf740f2556e
Okkar Min <OMIN001@e.ntu.edu.sg>
2020-03-15 08:20:59 +0800

    Extract mongoDBUrl to credentials.js and gitignored it

---
+++ credentials.js
@@ -0,0 +1,3 @@
+module.exports = {
+    mongoDBUrl: 'mongodb://cpf-neumorphic:qStpEFKhgtoODsZs94AVKlocKB6mE5GLf2lg4nN8aZrIHkroPCdxASEjWKe9nxu5DXJ8JlLWqsEr7O2izrCUiQ%3D%3D@cpf-neumorphic.mongo.cosmos.azure.com:10255/?ssl=true&appName=@cpf-neumorphic@',
+}
