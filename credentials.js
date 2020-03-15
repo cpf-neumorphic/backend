9f48401afe36e36dee602b28e804ead06cbacb2d
Okkar Min <OMIN001@e.ntu.edu.sg>
2020-03-16 07:20:00 +0800

    Extract mongoDB url to credentails.js file which is .gitignore

--- credentials.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-    mongoDBUrl: 'mongodb://cpf-neumorphic:qStpEFKhgtoODsZs94AVKlocKB6mE5GLf2lg4nN8aZrIHkroPCdxASEjWKe9nxu5DXJ8JlLWqsEr7O2izrCUiQ%3D%3D@cpf-neumorphic.mongo.cosmos.azure.com:10255/?ssl=true&appName=@cpf-neumorphic@',
-}
--- db.js
+++ db.js
@@ -1,5 +1,5 @@
 var MongoClient = require('mongodb').MongoClient;
-var url = 'mongodb://cpf-neumorphic:qStpEFKhgtoODsZs94AVKlocKB6mE5GLf2lg4nN8aZrIHkroPCdxASEjWKe9nxu5DXJ8JlLWqsEr7O2izrCUiQ%3D%3D@cpf-neumorphic.mongo.cosmos.azure.com:10255/?ssl=true&appName=@cpf-neumorphic@';
+var url = require('./credentials').mongoDBUrl
 
 let mongodb;
 
